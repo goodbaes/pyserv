cc7ef4e6a42cac9b92fbc63b525ceec6bf05cd46
goodbaes <95baes@gmail.com>
2020-09-19 10:38:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+
+const initCanvas = (id)=>{
+  return new fabric.Canvas(
+    id,{
+    width:500,
+    height:500,
+    selection:false});
+}
+const setBackground = (url,canvas) =>{
+   fabric.Image.fromURL(url,(img)=>{
+   canvas.backgroundImage = img;
+   canvas.renderAll();
+  })
+}
+
+const canvas = initCanvas('canvas');
+
+setBackground("https://cdn.pixabay.com/photo/2017/01/13/04/56/blank-1976334_960_720.png",canvas)
+
+let mousePressed = false;
+
+canvas.on("mouse:move",(event)=>{
+  if (mousePressed){
+    canvas.setCursor("grab");
+    canvas.renderAll();
+    const mEvent = event.e;
+    const delta = new fabric.Point(mEvent.movementX,mEvent.movementY);
+    canvas.relativePan(delta);
+  }
+})
+
+canvas.on('mouse:down',(event)=>{
+  mousePressed = true;
+  canvas.setCursor("grab");
+  canvas.renderAll()})
+
+canvas.on('mouse:up',(event)=>{
+  mousePressed = false;
+})
